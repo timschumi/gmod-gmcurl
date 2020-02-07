b69e0cfef2c157d7c497e53e65de9a3ef7d91e64
Tim Schumacher <timschumi@gmx.de>
2020-02-07 11:38:24 +0100

    examples: Add simple.lua

---
+++ examples/simple.lua
@@ -0,0 +1,19 @@
+require("curl")
+
+local curl = curl_easy_init()
+
+if not curl then
+	print("curl did not init!")
+	return
+end
+
+curl_easy_setopt(curl, CURLOPT_URL, "https://example.com")
+curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1)
+
+local res = curl_easy_perform(curl)
+
+if res != CURLE_OK then
+	print("curl_easy_perform() failed: "..curl_easy_strerror(res))
+end
+
+curl_easy_cleanup(curl)
